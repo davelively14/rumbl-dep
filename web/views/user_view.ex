6371abc7c0b30d7dac165f997f5896d6af964cd1
Dave Lively <davelively14@gmail.com>
2015-12-30 11:42:01 -0500

    Fixed spelling error

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,7 +1,7 @@
 defmodule Rumbl.UserView do
   use Rumbl.Web, :view
 
-  alias Rumble.User
+  alias Rumbl.User
 
   # Splits frist name from last, returns only first name
   def first_name(%User{name: name}) do
