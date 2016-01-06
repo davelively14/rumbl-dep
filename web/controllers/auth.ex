8d5d3052b5362cfd66b7530a1c45f9f997777248
Dave Lively <davelively14@gmail.com>
2016-01-06 15:01:24 -0500

    Created authorization plug

---
+++ web/controllers/auth.ex
@@ -0,0 +1,16 @@
+defmodule Rumbl.Auth do
+  import Plug.Conn
+
+  # Take given options and extracate the repository. If key does not exist, error
+  # will be raised. Rumbl.Auth will always require the :repo option.
+  def init(opts) do
+    Keyword.fetch!(opts, :repo)
+  end
+
+  # Will this run?
+  def call(conn, repo) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && repo.get(Rumbl.User, user_id)
+    assign(conn, :current_user, user)
+  end
+end
